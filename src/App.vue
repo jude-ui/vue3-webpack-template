dbce63215302d9dd37fad0b24023a75e14532cb0
Jude <jude@jude7230.local>
2022-06-23 12:32:26 +0900

    init

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <HelloWorld :msg="message" />
+  <HelloWorld msg="Good" />
+</template>
+
+<script>
+import HelloWorld from '~/components/HelloWorld'
+
+export default {
+  components: {
+    HelloWorld
+  },
+  data() {
+    return {
+      message: 'Hello Vue!'
+    }
+  }
+}
+</script>
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,30 @@
+<template>
+  <h2>{{ msg }}</h2> 
+  <img
+    src="~assets/logo_webpack.jpeg"
+    alt="HEROPY" />
+  <img
+    src="~assets/vue.png"
+    alt="vue" />
+  <h3>VUE3 WEBPACK TEMPLATE</h3>
+</template>
+
+<script>
+export default {
+  props: {
+    msg: {
+      type: String,
+      default: ''
+    }
+  }
+}
+</script>
+
+<style scoped lang="scss">
+  h2 {
+    font-size: 22px;
+    & ~ img {
+      width: 120px;
+    }
+  }
+</style>
